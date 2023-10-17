968a798db171ee8ef300c82f5a220462f4520129
Sándor Balázs <95277371+SandorBalazs0318@users.noreply.github.com>
2023-10-17 09:34:45 +0200

    Add files via upload

---
+++ javito.js
@@ -0,0 +1,15 @@
+
+    var oldalhatter = document.getElementById("oldal");
+    oldalhatter.style.backgroundColor = "#CCFFFF";
+
+    var containerhatter = document.getElementById("container");
+    oldalhatter.style.backgroundColor = "#12406A";
+
+    var dobozhatter1 = document.getElementById("szin1");
+    oldalhatter.style.backgroundColor = "#EB8F90";
+
+    var dobozhatter2 = document.getElementById("szin2");
+    oldalhatter.style.backgroundColor = "#FFB471";
+
+    var dobozhatter3 = document.getElementById("szin3");
+    oldalhatter.style.backgroundColor = "#ADBED2";
