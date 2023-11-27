1ccd6fd7d6d1bfa6b85f27cb8065e70960e56e1c
Sándor Balázs <95277371+SandorBalazs0318@users.noreply.github.com>
2023-11-27 10:31:35 +0100

    Add files via upload

---
+++ obj_1.js
@@ -0,0 +1,10 @@
+let obj = {fistName : "John", lastname: "Smith"}
+
+obj.fistName = "Paul";
+
+delete obj.fistName;
+
+function nev (){
+    console.log(obj.fistName + " " + obj.lastname + "" )
+};
+
